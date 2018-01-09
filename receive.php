48915f80e3264009664df05d3b2ebf8044502d5e
christine81420 <35244501+christine81420@users.noreply.github.com>
2018-01-09 11:43:49 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -2,7 +2,7 @@
   $json_str = file_get_contents('php://inoput'); //接收request
   $json_obj = json_decode($json_str); //轉JSON格式
 
-  $myfile = fopen("log.txt", "w+") or die("Unable eto open file!");
+  $myfile = fopen("log.txt", "w+") or die("Unable to open file!");
     fwrite($myfile, "\xEF\xBB\xBf".$json_str);
     fclose($myfile);
 ?>
